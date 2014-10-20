339461d1e4ba88f0837b427f455162ac99071241
Jorge Torres <jorgtowers@gmail.com>
2014-10-20 02:40:46 -0430

    Mejora Timer.js
    Se agrego la posibilidad de cambiar el intervalo en tiempo de ejecución para contar con la opción de repetir en Segundos, Minutos o en Horas, para que tome los cambios en línea solo debe afectar usando la propiedad "instancia.Reiniciar=true;"

--- Timer.js
+++ Timer.js
@@ -6,26 +6,66 @@
     // Variables
     var intervalo = Timer.prototype;
 	var activo = Timer.prototype;
+	var unidad = Timer.prototype;
+	var reiniciar=Timer.prototype;
+	var timerOn=null;
 
     // Métodos
     Timer.prototype.Constructor = function () {
 		this.intervalo = 0;
+		this.unidad = 0;
 		this.activo = false;
+		this.reiniciar=false;			
     }
 	Timer.prototype.Tick = function (callback) {
 		var seft=this;
-		setInterval(
-			function(){
-				if(seft.Activo)
+		var time=(this.Unidad===this.eUnidad.Segundos?1000:(this.Unidad===this.eUnidad.Minutos?60000:3600000))*this.Intervalo;
+		var timer;
+		(function loop() {
+			if(seft.Reiniciar){
+					time=(seft.Unidad===seft.eUnidad.Segundos?1000:(seft.Unidad===seft.eUnidad.Minutos?60000:3600000))*seft.Intervalo;
+					seft.Reiniciar=false;
+				}
+			if(seft.Activo)
 				{
 					if(typeof callback === 'function') {
 						callback();
 					}					
 				}
-			}, seft.Intervalo/1000);
+			timer = setTimeout(loop, time);
+		})();
     }
     
     //Propiedades
+	Object.defineProperty(Object.prototype,'Enum', {
+		value: function() {
+			for(i in arguments) {
+				Object.defineProperty(this,arguments[i], {
+					value:parseInt(i),
+					writable:false,
+					enumerable:true,
+					configurable:true
+				});
+			}
+			return this;
+		},
+		writable:false,
+		enumerable:false,
+		configurable:false
+	}); 
+	Object.defineProperty(Object.prototype, "eUnidad", {
+        get: function eUnidad() {
+            return {}.Enum('Segundos','Minutos','Horas');
+        }
+    });
+	Object.defineProperty(Timer.prototype, "Unidad", {
+        get: function Unidad() {
+            return unidad;
+        },
+        set: function Unidad(value) {
+            unidad = value;
+        }
+    });
     Object.defineProperty(Timer.prototype, "Intervalo", {
         get: function Intervalo() {
             return intervalo;
@@ -40,6 +80,14 @@
         },
         set: function Activo(value) {
             activo = value;
+        }
+    });
+	Object.defineProperty(Timer.prototype, "Reiniciar", {
+        get: function Reiniciar() {
+            return reiniciar;
+        },
+        set: function Reiniciar(value) {
+            reiniciar = value;
         }
     });
 
